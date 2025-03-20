f600c1a7a50c05b599fae5bd6fc26cd372f57662
Raj Rudani <rajrudani1234@gmail.com>
2025-03-20 10:29:46 +0530

    html setup

---
+++ resources/views/front/index-partials/about.blade.php
@@ -0,0 +1,68 @@
+<section id="about" class="about section">
+<!-- Section Title -->
+<div class="container section-title" data-aos="fade-up">
+    <h2>About</h2>
+    <p>Experienced Web Developer with 4+ years of expertise in architecting and implementing scalable web
+        applications using PHP, Laravel, CodeIgniter, JavaScript/jQuery, and MySQL. Specialized in e-commerce
+        and
+        fintech solutions, with a strong track record of leading projects, managing development teams, and
+        delivering
+        high-quality solutions on time. Passionate about performance optimization, secure coding practices, and
+        enhancing user experience. Adept at collaborating with cross-functional teams to develop robust,
+        scalable, and
+        efficient web applications.</p>
+</div><!-- End Section Title -->
+
+<div class="container" data-aos="fade-up" data-aos-delay="100">
+    <div class="row gy-4 justify-content-center">
+        <div class="col-lg-4">
+            <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-8 content">
+            <h2>Web Developer</h2>
+            <p class="fst-italic py-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et
+                dolore
+                magna aliqua.
+            </p>
+            <div class="row">
+                <div class="col-lg-6">
+                    <ul>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>24 September
+                                1998</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>LinkedIn:</strong> <span><a
+                                    href="https://www.linkedin.com/in/rajrudani/" target="_blank">Raj Rudani -
+                                    Profile</a></span>
+                        </li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+91
+                                (875)-806-5009</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Gujarat,
+                                India</span></li>
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <ul>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>26</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Bachelors in
+                                Information &
+                                Technology</span></li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong>
+                            <span>rajrudani1234@gmail.com</span>
+                        </li>
+                        <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong>
+                            <span>Available</span></li>
+                    </ul>
+                </div>
+            </div>
+            <p class="py-3">
+                Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci
+                omnis et
+                ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
+                Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque
+                neque.
+            </p>
+        </div>
+    </div>
+</div>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Front\IndexController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [IndexController::class, 'index'])->name('index');
