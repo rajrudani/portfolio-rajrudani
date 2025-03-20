b98ecb8a48e24d6326ca614279be76ef03ff5dad
Raj Rudani <rajrudani1234@gmail.com>
2025-03-20 17:27:26 +0530

    skills

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -9,6 +9,52 @@ class IndexController extends Controller
 {
     public function index()
     {
-        return view('front.index');
+        $skills = $this->getSkills();
+        
+        return view('front.index', compact('skills'));
+    }
+
+    public function getSkills ()
+    {
+        return [
+            // Programming Languages
+            ['name' => 'PHP', 'filter' => 'filter-programming', 'image' => asset('assets/img/skills/php.png')],
+            ['name' => 'HTML', 'filter' => 'filter-programming', 'image' => asset('assets/img/skills/html5.png')],
+            ['name' => 'CSS', 'filter' => 'filter-programming', 'image' => asset('assets/img/skills/css.png')],
+            ['name' => 'JavaScript', 'filter' => 'filter-programming', 'image' => asset('assets/img/skills/js.png')],
+        
+            // Frameworks & Libraries
+            ['name' => 'Laravel', 'filter' => 'filter-frameworks', 'image' => asset('assets/img/skills/laravel.png')],
+            ['name' => 'CodeIgniter', 'filter' => 'filter-frameworks', 'image' => asset('assets/img/skills/codeigniter.png')],
+            ['name' => 'SuiteCRM', 'filter' => 'filter-frameworks', 'image' => asset('assets/img/skills/suitecrm.png')],
+            ['name' => 'jQuery', 'filter' => 'filter-frameworks', 'image' => asset('assets/img/skills/jquery.png')],
+            ['name' => 'Ajax', 'filter' => 'filter-frameworks', 'image' => asset('assets/img/skills/ajax.png')],
+            ['name' => 'Bootstrap', 'filter' => 'filter-frameworks', 'image' => asset('assets/img/skills/bootstrap.png')],
+        
+            // Databases
+            ['name' => 'MySQL', 'filter' => 'filter-databases', 'image' => asset('assets/img/skills/mysql.png')],
+            ['name' => 'MongoDB', 'filter' => 'filter-databases', 'image' => asset('assets/img/skills/mongodb.png')],
+        
+            // APIs & Payment Gateways & Shipping & Logistics APIs
+            ['name' => 'Stripe', 'filter' => 'filter-apis', 'image' => asset('assets/img/skills/stripe.png')],
+            ['name' => 'Razorpay', 'filter' => 'filter-apis', 'image' => asset('assets/img/skills/razorpay.png')],
+            ['name' => 'CC Avenue', 'filter' => 'filter-apis', 'image' => asset('assets/img/skills/ccavenue.png')],
+            ['name' => 'Moneris', 'filter' => 'filter-apis', 'image' => asset('assets/img/skills/moneris.png')],
+            ['name' => 'Bitgo', 'filter' => 'filter-apis', 'image' => asset('assets/img/skills/bitgo.png')],
+            ['name' => 'Onmeta', 'filter' => 'filter-apis', 'image' => asset('assets/img/skills/onmeta.png')],
+            ['name' => 'Shiprocket', 'filter' => 'filter-apis', 'image' => asset('assets/img/skills/shiprocket.png')],
+            ['name' => 'Easypost', 'filter' => 'filter-apis', 'image' => asset('assets/img/skills/easypost.png')],
+            ['name' => 'AusPost', 'filter' => 'filter-apis', 'image' => asset('assets/img/skills/auspost.png')],
+            ['name' => 'UPS', 'filter' => 'filter-apis', 'image' => asset('assets/img/skills/ups.png')],
+            ['name' => 'USPS', 'filter' => 'filter-apis', 'image' => asset('assets/img/skills/usps.png')],
+            
+            // Other Technologies
+            ['name' => 'Shopify', 'filter' => 'filter-other', 'image' => asset('assets/img/skills/shopify.png')],
+            ['name' => 'GitHub', 'filter' => 'filter-other', 'image' => asset('assets/img/skills/github.png')],
+            ['name' => 'GitLab', 'filter' => 'filter-other', 'image' => asset('assets/img/skills/gitlab.png')],
+            ['name' => 'Linux', 'filter' => 'filter-other', 'image' => asset('assets/img/skills/linux.png')],
+            ['name' => 'Windows', 'filter' => 'filter-other', 'image' => asset('assets/img/skills/windows.png')],
+            ['name' => 'VS Code', 'filter' => 'filter-other', 'image' => asset('assets/img/skills/vscode.png')],
+        ];
     }
 }
--- resources/views/front/index-partials/skills.blade.php
+++ resources/views/front/index-partials/skills.blade.php
@@ -1,79 +1,176 @@
-<section id="skills" class="skills section">
+ <section id="skills" class="portfolio section">
+     <div class="container section-title" data-aos="fade-up">
+         <h2>Professional Skills</h2>
+         {{-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+            consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat
+            sit
+            in iste officiis commodi quidem hic quas.</p> --}}
+     </div>
 
-    <!-- Section Title -->
-    <div class="container section-title" data-aos="fade-up">
-        <h2>Skills</h2>
-        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
-    </div><!-- End Section Title -->
+     <div class="container">
 
-    <div class="container" data-aos="fade-up" data-aos-delay="100">
+         <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
+             <ul class="portfolio-filters isotope-filters" data-aos="fade-up" data-aos-delay="100">
+                 <li data-filter="*" class="filter-active">All</li>
+                 <li data-filter=".filter-programming">Programming Languages</li>
+                 <li data-filter=".filter-frameworks">Frameworks</li>
+                 <li data-filter=".filter-databases">Databases</li>
+                 <li data-filter=".filter-apis">Third Party APIs</li>
+                 <li data-filter=".filter-other">Other</li>
+             </ul>
 
-        <div class="row skills-content skills-animation">
+             <div class="row mt-3 gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-programming">
+                    <img src="{{ asset('assets/img/skills/php.png') }}" height="70" width="70" class="img-fluid"
+                        alt="">
+                    <h6>PHP</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-programming">
+                     <img src="{{ asset('assets/img/skills/html5.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>HTML</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-programming">
+                     <img src="{{ asset('assets/img/skills/css.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>CSS</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-programming">
+                     <img src="{{ asset('assets/img/skills/js.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Javascript</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-frameworks">
+                     <img src="{{ asset('assets/img/skills/laravel.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Laravel</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-frameworks">
+                     <img src="{{ asset('assets/img/skills/codeigniter.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Codeigniter</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-frameworks">
+                     <img src="{{ asset('assets/img/skills/suitecrm.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>SuiteCRM</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-frameworks">
+                     <img src="{{ asset('assets/img/skills/jquery.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>JQuery</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-frameworks">
+                     <img src="{{ asset('assets/img/skills/ajax.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Ajax</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-frameworks">
+                     <img src="{{ asset('assets/img/skills/bootstrap.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Bootstrap</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-databases">
+                     <img src="{{ asset('assets/img/skills/mysql.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>MySQL</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-databases">
+                     <img src="{{ asset('assets/img/skills/mongodb.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>MongoDB</h6>
+                 </div>
 
-            <div class="col-lg-6">
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-apis">
+                     <img src="{{ asset('assets/img/skills/stripe.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Stripe</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-apis">
+                     <img src="{{ asset('assets/img/skills/razorpay.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Razorpay</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-apis">
+                     <img src="{{ asset('assets/img/skills/ccavenue.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>CC Avenue</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-other">
+                     <img src="{{ asset('assets/img/skills/shopify.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Shopify</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-other">
+                     <img src="{{ asset('assets/img/skills/github.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>GitHub</h6>
+                 </div>
 
-                <div class="progress">
-                    <span class="skill"><span>HTML</span> <i class="val">100%</i></span>
-                    <div class="progress-bar-wrap">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0"
-                            aria-valuemax="100">
-                        </div>
-                    </div>
-                </div><!-- End Skills Item -->
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-other">
+                     <img src="{{ asset('assets/img/skills/gitlab.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>GitLab</h6>
+                 </div>
 
-                <div class="progress">
-                    <span class="skill"><span>CSS</span> <i class="val">90%</i></span>
-                    <div class="progress-bar-wrap">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0"
-                            aria-valuemax="100">
-                        </div>
-                    </div>
-                </div><!-- End Skills Item -->
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-apis">
+                     <img src="{{ asset('assets/img/skills/moneris.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Moneris</h6>
+                 </div>
 
-                <div class="progress">
-                    <span class="skill"><span>JavaScript</span> <i class="val">75%</i></span>
-                    <div class="progress-bar-wrap">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
-                            aria-valuemax="100">
-                        </div>
-                    </div>
-                </div><!-- End Skills Item -->
+                 {{-- <div class="col-md-2 text-center portfolio-item isotope-item filter-apis">
+                    <img src="{{ asset('assets/img/skills/auspost.png') }}" height="120px" width="120px" class="img-fluid"
+                        alt="">
+                        <h6>CSS</h6>
+                </div> --}}
 
-            </div>
-
-            <div class="col-lg-6">
-
-                <div class="progress">
-                    <span class="skill"><span>PHP</span> <i class="val">80%</i></span>
-                    <div class="progress-bar-wrap">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0"
-                            aria-valuemax="100">
-                        </div>
-                    </div>
-                </div><!-- End Skills Item -->
-
-                <div class="progress">
-                    <span class="skill"><span>WordPress/CMS</span> <i class="val">90%</i></span>
-                    <div class="progress-bar-wrap">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0"
-                            aria-valuemax="100">
-                        </div>
-                    </div>
-                </div><!-- End Skills Item -->
-
-                <div class="progress">
-                    <span class="skill"><span>Photoshop</span> <i class="val">55%</i></span>
-                    <div class="progress-bar-wrap">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0"
-                            aria-valuemax="100">
-                        </div>
-                    </div>
-                </div><!-- End Skills Item -->
-
-            </div>
-
-        </div>
-
-    </div>
-
-</section>
+                 {{-- <div class="col-md-2 text-center portfolio-item isotope-item filter-apis">
+                    <img src="{{ asset('assets/img/skills/ups.png') }}" height="175px" width="175px" class="img-fluid mt-4"
+                        alt="">
+                        <h6>CSS</h6>
+                </div> --}}
+                 {{-- <div class="col-md-2 text-center portfolio-item isotope-item filter-apis">
+                    <img src="{{ asset('assets/img/skills/usps.png') }}" height="125px" width="125px" class="img-fluid mt-3"
+                        alt="">
+                        <h6>CSS</h6>
+                </div> --}}
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-apis">
+                     <img src="{{ asset('assets/img/skills/bitgo.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Bitgo</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-apis">
+                     <img src="{{ asset('assets/img/skills/onmeta.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Onmeta</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-other">
+                     <img src="{{ asset('assets/img/skills/linux.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Linux</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-apis">
+                     <img src="{{ asset('assets/img/skills/shiprocket.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Shiprocket</h6>
+                 </div>
+                 <div class="col-md-2 text-center portfolio-item isotope-item filter-apis">
+                     <img src="{{ asset('assets/img/skills/easypost.png') }}" height="70" width="70"
+                         class="img-fluid" alt="">
+                     <h6>Easypost</h6>
+                 </div>
+                 {{-- <div class="col-md-2 text-center portfolio-item isotope-item filter-other">
+                    <img src="{{ asset('assets/img/skills/windows.png') }}" height="70" width="70" class="img-fluid mt-3"
+                        alt="">
+                        <h6>CSS</h6>
+                </div>
+                <div class="col-md-2 text-center portfolio-item isotope-item filter-other">
+                    <img src="{{ asset('assets/img/skills/vscode.png') }}" height="70" width="70" class="img-fluid mt-3"
+                        alt="">
+                        <h6>CSS</h6>
+                </div> --}}
+             </div>
+         </div>
+     </div>
+ </section>
